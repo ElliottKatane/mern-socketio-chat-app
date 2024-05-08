935d46d26b0f00900efad7e5ca1860d3ced7c494
Elliott Katane <elliott.katane@gmail.com>
2024-05-08 09:58:07 -0400

    deployment

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,4 @@
+import path from "path";
 import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
@@ -10,7 +11,7 @@ import { app, server } from "./socket/sockets.js";
 dotenv.config();
 // consts
 const PORT = process.env.PORT || 5000;
-
+const __dirname = path.resolve();
 //A placer avant les routes (app.use /api/auth...), pour que le serveur puisse parser le body (req.body)
 app.use(express.json());
 app.use(cookieParser());
@@ -19,6 +20,11 @@ app.use("/api/auth", authRoute);
 app.use("/api/messages", messageRoute);
 app.use("/api/users", userRoute);
 
+app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
+});
 server.listen(PORT, () => {
   connectToMongoDB();
   console.log(`Server is running on port ${PORT}`);
